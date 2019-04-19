5780b61833bc28a0bcab12e2d2fda8bb29bda1cf
David Guben <davidjg6@gmail.com>
2019-04-19 12:52:43 -0400

    turned cards into a grid, added animations to each element, resized responsive layouts, added onclick events for flipping the card over

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,29 @@
+
+// Hide navigation list when its not displayed
 function toggleNaviDisplay () {
     let x = document.getElementById('navi-list');
 
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
-        setTimeout(x.style.display = "none", 5000);
+        x.style.display = "none";
     }
+}
+
+// toggle flipping the card
+function flipCard() {
+    let x = document.getElementById('card__side--front');
+    let y = document.getElementById('card__side--back');
+
+    x.style.transform = "rotateY(-180deg)";
+    y.style.transform = "rotateY(0)";
+}
+
+// toggle flipping the card back
+function flipCardBack() {
+    let x = document.getElementById('card__side--front');
+    let y = document.getElementById('card__side--back');
+    
+    x.style.transform = "rotateY(0)";
+    y.style.transform = "rotateY(-180deg)";
 }
