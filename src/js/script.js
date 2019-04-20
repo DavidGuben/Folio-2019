dabdc9ab0711160d20bda92b8c83ba068eece1ab
David Guben <davidjg6@gmail.com>
2019-04-20 00:45:51 -0400

    overhaul on card, added new style and animations. now overflow of img on hover, btn on click flips card

--- src/js/script.js
+++ src/js/script.js
@@ -11,18 +11,108 @@ function toggleNaviDisplay () {
 }
 
 // toggle flipping the card
-function flipCard() {
-    let x = document.getElementById('card__side--front');
-    let y = document.getElementById('card__side--back');
+function flipCard1() {
+    let x = document.getElementById('card__side--front-1');
+    let y = document.getElementById('card__side--back-1');
 
     x.style.transform = "rotateY(-180deg)";
     y.style.transform = "rotateY(0)";
 }
 
 // toggle flipping the card back
-function flipCardBack() {
-    let x = document.getElementById('card__side--front');
-    let y = document.getElementById('card__side--back');
+function flipCardBack1() {
+    let x = document.getElementById('card__side--front-1');
+    let y = document.getElementById('card__side--back-1');
+    
+    x.style.transform = "rotateY(0)";
+    y.style.transform = "rotateY(-180deg)";
+}
+
+// toggle flipping the card
+function flipCard2() {
+    let x = document.getElementById('card__side--front-2');
+    let y = document.getElementById('card__side--back-2');
+
+    x.style.transform = "rotateY(-180deg)";
+    y.style.transform = "rotateY(0)";
+}
+
+// toggle flipping the card back
+function flipCardBack2() {
+    let x = document.getElementById('card__side--front-2');
+    let y = document.getElementById('card__side--back-2');
+    
+    x.style.transform = "rotateY(0)";
+    y.style.transform = "rotateY(-180deg)";
+}
+
+// toggle flipping the card
+function flipCard3() {
+    let x = document.getElementById('card__side--front-3');
+    let y = document.getElementById('card__side--back-3');
+
+    x.style.transform = "rotateY(-180deg)";
+    y.style.transform = "rotateY(0)";
+}
+
+// toggle flipping the card back
+function flipCardBack3() {
+    let x = document.getElementById('card__side--front-3');
+    let y = document.getElementById('card__side--back-3');
+    
+    x.style.transform = "rotateY(0)";
+    y.style.transform = "rotateY(-180deg)";
+}
+
+// toggle flipping the card
+function flipCard4() {
+    let x = document.getElementById('card__side--front-4');
+    let y = document.getElementById('card__side--back-4');
+
+    x.style.transform = "rotateY(-180deg)";
+    y.style.transform = "rotateY(0)";
+}
+
+// toggle flipping the card back
+function flipCardBack4() {
+    let x = document.getElementById('card__side--front-4');
+    let y = document.getElementById('card__side--back-4');
+    
+    x.style.transform = "rotateY(0)";
+    y.style.transform = "rotateY(-180deg)";
+}
+
+// toggle flipping the card
+function flipCard5() {
+    let x = document.getElementById('card__side--front-5');
+    let y = document.getElementById('card__side--back-5');
+
+    x.style.transform = "rotateY(-180deg)";
+    y.style.transform = "rotateY(0)";
+}
+
+// toggle flipping the card back
+function flipCardBack5() {
+    let x = document.getElementById('card__side--front-5');
+    let y = document.getElementById('card__side--back-5');
+    
+    x.style.transform = "rotateY(0)";
+    y.style.transform = "rotateY(-180deg)";
+}
+
+// toggle flipping the card
+function flipCard6() {
+    let x = document.getElementById('card__side--front-6');
+    let y = document.getElementById('card__side--back-6');
+
+    x.style.transform = "rotateY(-180deg)";
+    y.style.transform = "rotateY(0)";
+}
+
+// toggle flipping the card back
+function flipCardBack6() {
+    let x = document.getElementById('card__side--front-6');
+    let y = document.getElementById('card__side--back-6');
     
     x.style.transform = "rotateY(0)";
     y.style.transform = "rotateY(-180deg)";
